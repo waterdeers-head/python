2a9379fa518c8c3646e2131b88e6f4367adea58c
waterdeers-head <leetaegyeong1012@gmail.com>
2025-06-03 13:53:30 +0900

    Add files via upload

--- name.py
+++ name.py
@@ -5,40 +5,23 @@ import time
 name = "이태경"
 colors = ["#f30000", "#fc7703", "#f2cb08", "#43f308", "#08c8f3", "#2308f3", "#7907f2"]
 
-num_names = 4
-
 if "step" not in st.session_state:
     st.session_state.step = 0
 
 placeholder = st.empty()
 
-
-html_code = """
-<div style='
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 20px;
-    justify-items: center;
-    align-items: center;
-    text-align: center;
-'>
-"""
+html_code = '<div style="display: grid; grid-template-columns: 1fr 1fr; gap: 20px; justify-items: center; align-items: center; text-align: center;">'
 
 for _ in range(4):
     color = random.choice(colors)
-    html_code += f"""
-    <h1 style='
-        margin: 10px;
-        color: {color};
-        font-size: 80px;
-    '>{name}</h1>
-    """
+    html_code += f'<h1 style="margin: 10px; color: {color}; font-size: 80px;">{name}</h1>'
 
-html_code += "</div>"
-
+html_code += '</div>'
 
 placeholder.markdown(html_code, unsafe_allow_html=True)
 
-time.sleep(0.01)
+time.sleep(0.001)
 st.session_state.step += 1
 st.rerun()
+
+
